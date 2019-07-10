166d14c3bf198912876e19f78e4895fbd0fc79fb
Laurentiu Marinovici <laurmarinovici@gmail.com>
2019-07-09 22:39:47 -0700

    ====

--- jmodelica/testcase.py
+++ jmodelica/testcase.py
@@ -15,11 +15,13 @@ This module defines the API to the test case used by the REST requests to
 perform functions such as advancing the simulation, retreiving test case 
 information, and calculating and reporting results.
 """
-
-from pyfmi import load_fmu
-import numpy as np
-import copy
-import config
+try:
+  from pyfmi import load_fmu
+  import numpy as np
+  import copy
+  import config
+except ImportError:
+  pass
 
 class TestCase(object):
     '''Class that implements the test case.
