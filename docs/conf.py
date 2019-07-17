058132f4c19084c09811a9cab9c0b8e49c2ebbaa
Laurentiu Marinovici <laurmarinovici@gmail.com>
2019-07-17 07:50:38 -0700

    ===

--- docs/conf.py
+++ docs/conf.py
@@ -67,4 +67,4 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-autodoc_mock_imports = ["flask", "flask_restful", "pyfmi", "numpy", "copy"]
+autodoc_mock_imports = ["flask", "flask_restful", "pyfmi", "numpy", "copy", "testcase"]
