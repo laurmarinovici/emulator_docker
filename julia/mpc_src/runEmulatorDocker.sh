b9fd0eab3fac4d1e4d957eb423858919c53198e9
Laurentiu Marinovici <laurmarinovici@gmail.com>
2019-07-23 13:43:20 -0700

    updating the jmodelica docker

--- julia/mpc_src/runEmulatorDocker.sh
+++ julia/mpc_src/runEmulatorDocker.sh
@@ -3,4 +3,4 @@
 docker run -it --rm -p="127.0.0.1:5000:5000" \
            --mount type=bind,source=/Users/mari009/PNNL_Projects/GitHubRepositories/emulator_docker/jmodelica/,destination=/mnt/master \
            --mount type=bind,source=/Users/mari009/PNNL_Projects/GitHubRepositories/emulator_docker_fork/jmodelica/,destination=/mnt/fork \
-           --network=host --name=jmodelica boptest_testcase3 bash
+           --network=host --name=jmodelica_docker building_control_emulator bash
