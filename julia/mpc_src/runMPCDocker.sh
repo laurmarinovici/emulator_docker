187a3625e6afb9041b516e9db2ceb5f383fc39da
Laurentiu Marinovici <laurmarinovici@gmail.com>
2019-07-23 15:33:04 -0700

    bring quantum running scripts to repo

--- julia/mpc_src/runMPCDocker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker run -it --rm -p="127.0.0.1:5000:5000" \
-           --mount type=bind,source=/Users/mari009/PNNL_Projects/GitHubRepositories/emulator_docker/julia,destination=/mnt/myapp \
-           --mount type=bind,source=/Users/mari009/PNNL_Projects/GitHubRepositories/emulator_docker_fork/julia/mpc_src,destination=/mnt/mpc \
-           --network=host --name julia_container julia_image:latest bash
