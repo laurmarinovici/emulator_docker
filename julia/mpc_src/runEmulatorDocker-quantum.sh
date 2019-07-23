bfc0648b2ba08eed0d25c8f2978959866d0653aa
Laurentiu Marinovici <laurmarinovici@gmail.com>
2019-07-23 15:42:17 -0700

    ===

--- julia/mpc_src/runEmulatorDocker-quantum.sh
+++ julia/mpc_src/runEmulatorDocker-quantum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker run -it --rm -p="127.0.0.1:5000:5000" \
-           --mount type=bind,source=/home/tesp/repositories/emulator_docker/jmodelica/,destination=$HOME/master \
-           --mount type=bind,source=/home/tesp/repositories/emulator_docker_fork/jmodelica/,destination=$HOME/fork \
+           --mount type=bind,source=/home/tesp/repositories/emulator_docker/jmodelica/,destination=/mnt/master \
+           --mount type=bind,source=/home/tesp/repositories/emulator_docker_fork/jmodelica/,destination=/mnt/fork \
            --network=host --name=jmodelica_docker laurmarinovici/building_control_emulator:latest bash
