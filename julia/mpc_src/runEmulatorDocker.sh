a2f9cac691f34d461986e60f29e5c2dddf612ace
Laurentiu Marinovici <laurmarinovici@gmail.com>
2019-07-25 13:28:27 -0700

    deleting the mpc sources. see you on stash only

--- julia/mpc_src/runEmulatorDocker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run -it --rm -p="127.0.0.1:5000:5000" \
-           --mount type=bind,source=/Users/mari009/PNNL_Projects/GitHubRepositories/emulator_docker_fork/emulatorExamples/,destination=/mnt/examples \
-           --name=jmodelica_docker laurmarinovici/building_control_emulator:latest bash
-
-# --network=host 
--- julia/mpc_src/runMPCDocker-quantum.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker run -it --rm -p="127.0.0.1:5000:5000" \
-           --mount type=bind,source=/home/tesp/repositories/emulator_docker_fork/julia/,destination=/mnt/myapp \
-           --mount type=bind,source=/home/tesp/repositories/emulator_docker_fork/julia/mpc_src/,destination=/mnt/mpc \
-           --network=host --name julia_docker laurmarinovici/julia_1.1.1:latest bash
